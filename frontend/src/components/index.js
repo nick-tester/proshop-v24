43a43af27f334935146c817d0773354e3c2fc778
Romenick <romenick.tester@gmail.com>
2024-02-20 23:52:04 +0000

    @frontend: create and mount rating component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Product from "./Product";
 import Main from "./Main";
+import Rating from "./Rating";
 
 export {
     Header,
     Footer,
     Product,
     Main,
+    Rating,
 };
