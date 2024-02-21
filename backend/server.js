9979dabb1f9042f0d5d0e680641d6888798f8d8b
Romenick <romenick.tester@gmail.com>
2024-02-21 17:40:25 +0000

    create products api endpoint and single product api endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import products from "./data/products.js";
 const PORT = 4000;
 
 
@@ -6,6 +7,15 @@ const server = express();
 
 server.get("/", (req, res) => {
     res.send("Root endpoint running...");
+});
+
+server.get("/api/v24/products", (req, res) => {
+    res.json(products);
+});
+
+server.get("/api/v24/products/:id", (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
 });
 
 
