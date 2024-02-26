f69edb08229cdde5bba83f3f06e0dbd0141a7441
Romenick <romenick.tester@gmail.com>
2024-02-26 11:43:58 +0000

    @backend: create error handlers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middlewares/errorHandlers.js";
 
 
 connectDB();
@@ -17,6 +18,9 @@ server.get("/", (req, res) => {
 
 server.use("/api/v24/products", productRoutes);
 
+server.use(notFound);
+
+server.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
