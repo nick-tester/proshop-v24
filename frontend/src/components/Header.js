e3c2c3c54a1269885589ccb8cf11a0deeaf40b4a
Romenick Tester <romenick.tester@gmail.com>
2024-03-11 13:20:04 +0000

    frontend - header component - display product cart qty badge

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Navbar, Nav, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer as Link } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
 import logo from "../assets/logo.png";
 
 
 const Header = () => {
+    const { cartItems } = useSelector(state => state.cart);
+
     return <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
@@ -20,7 +23,14 @@ const Header = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Link to="/cart">
-                            <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                            <Nav.Link>
+                                <FaShoppingCart /> Cart
+                                {cartItems.length > 0 && (
+                                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                                        {cartItems.reduce((a, i) => a + i.qty, 0)}
+                                    </Badge>
+                                )}
+                            </Nav.Link>
                         </Link>
                         <Link to="/user">
                             <Nav.Link><FaUser /> Log In</Nav.Link>
