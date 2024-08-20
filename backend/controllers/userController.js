396ce7f09c389166909c242d06db2af5025d05a9
Romenick <romenick.tester@gmail.com>
2024-08-20 10:53:24 +0100

    @backend: user controller route - auth user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,26 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
-// import User from "../models/userModel.js";
+import User from "../models/userModel.js";
 
 // @desc    Auth user and get token  
 // @route   POST /api/v24/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.send("Auth user");
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const server = express();
 
 server.use(express.json());
 
+server.use(express.urlencoded({ extended: true }));
+
 server.get("/", (req, res) => {
     res.send("Root endpoint running...");
 });
